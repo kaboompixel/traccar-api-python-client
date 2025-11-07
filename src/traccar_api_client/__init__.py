483c31715c5ac58406508dfc8732ba48c15279a6
kaboompixel <223810+kaboompixel@users.noreply.github.com>
2025-11-07 12:20:45 +0100

    feat: add generated client [Traccar 6.1.0] (#17)
    * chore: remove boilerplate
    
    * chore: set specific python version for CI
    
    * feat: add a copy of the openapi spec for traccar, version 6.1.0
    
    * feat: add the generated client code
    
    * feat: add a simple unit test for the generated client
    
    * feat: add config for openapi-python-client
    
    * chore: update pyproject file, adding package requirements and python versions
    
    * chore: adjust linting rules to work with auto-generated code. Note: these rules are fairly lenient.
    
    * chore: flake8 to ignore long docstring lines
    
    * chore: add a placeholder integration test to ensure there is at least 1

--- src/traccar_api_client/__init__.py
+++ src/traccar_api_client/__init__.py
@@ -1,8 +1,10 @@
-#   -------------------------------------------------------------
-#   Copyright (c) Microsoft Corporation. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   -------------------------------------------------------------
-"""Python Package Template"""
-from __future__ import annotations
+"""A client library for accessing Traccar"""
 
-__version__ = "0.0.2"
+__version__ = "0.0.1"
+
+from .client import AuthenticatedClient, Client
+
+__all__ = (
+    "AuthenticatedClient",
+    "Client",
+)
